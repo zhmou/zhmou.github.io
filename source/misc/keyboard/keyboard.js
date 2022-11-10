f7e1f691fba390e0ba5660bce290190e21d33005
zhmou <948508913@qq.com>
2022-11-10 19:16:54 +0800

    fix keyboard test

--- source/misc/keyboard/keyboard.js
+++ source/misc/keyboard/keyboard.js
@@ -1,15 +1,19 @@
 let body = document.body;
 let keys = document.getElementsByClassName('key');
 let textArea = document.getElementById('Keylog');
+let btn = document.querySelector('button');
+
 body.addEventListener('keydown', keydown_listener);
 body.addEventListener('keyup', keyup_listener);
-
+body.addEventListener('click', clearAll);
 function keydown_listener(e) {
     e.preventDefault();
     let ele;
     ele = document.getElementById(e.code);
     if (ele.className.indexOf(' active') === -1) {
         ele.className += ' active';
+    } else {
+        return false;
     }
 
     textArea.value = time() + '你按下了' + e.code + '键\n' + textArea.value;
@@ -25,6 +29,14 @@ function keyup_listener(e) {
     ele.querySelector('.before').style.background = "linear-gradient(90deg, #fff888, #fffbbb)";
 }
 
+function clearAll() {
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].className.replace(' active', '');
+        keys[i].querySelector('.before').style.background = "";
+    }
+    textArea.value = '';
+}
+
 function time() {
     let date = new Date();
     let h = date.getHours();
@@ -37,5 +49,10 @@ function time() {
         s = '0' + s;
     }
     let ms = date.getMilliseconds();
+    if (ms < 10) {
+        ms = '00' + ms;
+    } else if (ms < 100) {
+        ms = '0' + ms;
+    }
     return h + ':' + m + ':' + s + '.' + ms + '\t';
 }
