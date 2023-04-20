03c9606a1b37a52bf2221aa43d6c5918a4538c26
zhmou <948508913@qq.com>
2023-04-20 17:50:07 +0800

    23/4/20

--- source/misc/pinyougou/js/focus.js
+++ source/misc/pinyougou/js/focus.js
@@ -1,50 +1,71 @@
 let wrapper = document.querySelector('.wrapper');
+wrapper.appendChild(wrapper.children[0].cloneNode(true));
 let dots = document.querySelector('.dots');
-wrapper.style.width = wrapper.childElementCount * wrapper.firstElementChild.offsetWidth + 'px';
-
-let minThershold = 0;
-let maxThershold = - (wrapper.childElementCount - 1) * wrapper.firstElementChild.offsetWidth;
-
+let current = 0;
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
-
-prevButton.addEventListener('click', ()=>{
-    const currLeft = parseInt(getComputedStyle(wrapper).left.replace('px', ''));
-    if (currLeft != 0) {
-        move(wrapper, 700 + currLeft, whichSelected);
-    }
-})
-
-nextButton.addEventListener('click', ()=>{
-    const currLeft = parseInt(getComputedStyle(wrapper).left.replace('px', ''));
-    if (currLeft != maxThershold) {
-        move(wrapper, -700 + currLeft, whichSelected);
-    }
-})
-
-let dot = [];
-for (let i = 0; i < wrapper.childElementCount; i++) {
-    dot[i] = '<li class="dot"></li>';
+for (let i = 0; i < wrapper.childElementCount - 1; i++) {
+    let li = document.createElement('li');
+    li.classList.add('dot');
+    li.setAttribute('data-index', i);
+    li.addEventListener('click', function() {
+        for (let j = 0; j < dots.children.length; j++) {
+            dots.children[j].classList.remove('selected');
+        }
+        this.classList.add('selected');
+        move(wrapper, - li.getAttribute('data-index') * wrapper.firstElementChild.offsetWidth);
+        current = parseInt(li.getAttribute('data-index'));
+    }) 
+    dots.appendChild(li);
 }
+dots.children[0].classList.add('selected');
+wrapper.style.width = wrapper.childElementCount * wrapper.firstElementChild.offsetWidth + 'px';
+let timer = setInterval(()=>{
+    nextButton.click();
+}, 2000);
 
-dots.innerHTML = dot.join("");
+wrapper.addEventListener('mouseenter', () => {
+    clearInterval(timer)
+})
 
-window.addEventListener('DOMContentLoaded', whichSelected);
+wrapper.addEventListener('mouseleave', () => {
+    timer = setInterval(()=>{
+        nextButton.click();
+    }, 2000);
+})
 
-function whichSelected() {
-    const currLeft = parseInt(getComputedStyle(wrapper).left.replace('px', ''));
-    for (let i=0; i < dots.children.length; i++) {
+
+prevButton.addEventListener('click', prev);
+
+nextButton.addEventListener('click', next);
+
+function prev() {
+    if (current == 0) {
+        wrapper.style.left = - (wrapper.childElementCount - 1) * wrapper.firstElementChild.offsetWidth + 'px';
+        current = wrapper.childElementCount - 1;
+    }
+    current = current - 1;
+    for (let i = 0; i < dots.children.length; i++) {
         dots.children[i].classList.remove('selected');
     }
-    dots.children[-currLeft / wrapper.firstElementChild.offsetWidth].classList.add('selected');
+    move(wrapper, - current * wrapper.firstElementChild.offsetWidth);
+    if (current == wrapper.childElementCount - 1) {
+        current = 0;
+    }
+    dots.children[current].classList.add('selected');
 }
 
-dots.addEventListener('click', e => {
-    if (e.target.classList == 'dot') {
-        for (let i = 0; i < dots.children.length; i++) {
-            if (e.target == dots.children[i]) {
-                move(wrapper, -700 * i, whichSelected);
-            }
-        }
+function next() {
+    if (current == 0) {
+        wrapper.style.left = '0px';
     }
-})
+    current = current + 1;
+    for (let i = 0; i < dots.children.length; i++) {
+        dots.children[i].classList.remove('selected');
+    }
+    move(wrapper, - current * wrapper.firstElementChild.offsetWidth);
+    if (current == wrapper.childElementCount - 1) {
+        current = 0;
+    }
+    dots.children[current].classList.add('selected');
+}
