b093107d4ea6fa6067623600af206fed3ffb4aa2
zhmou <948508913@qq.com>
2023-05-04 20:34:51 +0800

    23/5/4

--- source/misc/xc/js/index.js
+++ source/misc/xc/js/index.js
@@ -1,14 +1,14 @@
-window.onload = function () {
+window.addEventListener('load', function () {
     let i = 0;
     const banner = document.querySelector('.banner_img');
     const dots = document.querySelector('.dots');
     const w = document.querySelector('.focus').offsetWidth;
     const max = dots.children.length;
-    banner.timer = setInterval(function () {
-        i++;
-        banner.style.transition = 'all 1s';
-        banner.style.transform = `translateX(${-i * w}px)`;
-    }, 2000);
+    const goBack = document.querySelector('.goBack');
+    const nav = document.querySelector('.nav');
+    console.log(goBack);
+
+    banner.timer = setInterval(banner_timer, 2000);
 
     banner.addEventListener('transitionend', function () {
         if (i == max) {
@@ -22,5 +22,56 @@ window.onload = function () {
         }
         dots.querySelector('li.current').classList.remove('current');
         dots.children[i].classList.add('current');
+    });
+
+    let startX, moveX = 0;
+    banner.addEventListener('touchstart', function (e) {
+        startX = e.targetTouches[0].pageX;
+        clearInterval(this.timer);
+    });
+    banner.addEventListener('touchmove', function (e) {
+        moveX = e.targetTouches[0].pageX - startX;
+        banner.style.transition = '';
+        banner.style.transform = `translateX(${-i * w + moveX}px)`;
+        e.preventDefault();
     })
-}
+
+    banner.addEventListener('touchend', function (e) {
+        if (Math.abs(moveX) > 50) {
+            if (moveX > 0) {
+                i--;
+            } else {
+                i++;
+            }
+        }
+        banner.style.transition = 'all 1s';
+        banner.style.transform = `translateX(${-i * w}px)`;
+        banner.timer = setInterval(banner_timer, 2000);
+    })
+
+    function banner_timer() {
+        i++;
+        banner.style.transition = 'all 1s';
+        banner.style.transform = `translateX(${-i * w}px)`;
+    }
+
+    this.document.addEventListener('visibilitychange', function () {
+        if (document.hidden && banner.timer) {
+            clearInterval(banner.timer);
+        } else {
+            banner.timer = setInterval(banner_timer, 2000);
+        }
+    })
+
+    this.addEventListener('scroll', function () {
+        if (this.scrollY >= nav.offsetTop) {
+            goBack.style.display = 'block';
+        } else {
+            goBack.style.display = '';
+        }
+    });
+
+    goBack.addEventListener('click', function () {
+        window.scroll(0, 0);
+    })
+})
